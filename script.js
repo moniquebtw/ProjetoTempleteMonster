add317c103e3146d3dca403b263ee26e1417fb0a
moniquebtw <moniquesilva14321@gmail.com>
2025-03-28 17:23:43 -0300

    home

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const iconesServices = ['mobile.jpg', 'website_designing.jpg', 'graphic.jpg', 'd
 const titulosDosIconesServices = ['Mobile App Designing', 'Website Designing', 'Graphic Designing', 'Digital Marketing']
 const textoDosIconesServices = ['Justo, amet nisl velit quam. Turpis nulla morbi vestibulum morbi cum et.', 'Eu aliquam libero vehicula posuere. Orci viverra id pharetra adipiscing. Convallis.', 'Aliquam ut sapien viverra blandit nisi. Nam fames suscipit erat sed id. Risus.', 'Pulvinar amet ac potenti quam feugiat praesent maecenas. Platea id egestas.']
 const companhias = ['company-1.jpg', 'company-2.jpg', 'company-3.jpg', 'company-4.jpg', 'company-5.jpg', 'company-6.jpg', 'company-7.jpg', 'company-8.jpg', 'company-9.jpg', 'company-10.jpg', 'company-11.jpg']
+const nossoTrabalho = ['blog_1.jpg', 'blog_2.jpg', 'blog_3.jpg'];
+
 const variosIconesServices = document.querySelector('.card')
 const variasCompanhias = document.querySelector('.companies_list')
+const conhecaNossoTrabalho = document.getElementById('conhecaNossoTrabalho')
+const estrelas = document.getElementById('estrelas')
 
 for (let i = 0; i < 4; i++) {
     const novoCard = document.createElement('div');
@@ -25,5 +29,26 @@ for (let i = 0; i < 11; i++) {
     `;
 
     variasCompanhias.appendChild(novaCompanhia);
+}
+
+for(let i = 0; i < 3; i++) {
+    const novoItem = document.createElement('div');
+    novoItem.className = ('novoItem');
+    novoItem.innerHTML = `
+
+                <img src="/assets/${nossoTrabalho[i]}" alt="${nossoTrabalho[i]}">
+    `;
+
+    conhecaNossoTrabalho.appendChild(novoItem);
+}
+
+for (let i = 0; i < 5; i++) {
+    const novaEstrela = document.createElement('i');
+    novaEstrela.className = ('estrela');
+    novaEstrela.innerHTML = `
+        <i class="fa fa-star" aria-hidden="true"></i>
+    `;
+
+    estrelas.appendChild(novaEstrela);
 }
 
