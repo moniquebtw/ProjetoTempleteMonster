2a218b39d7531c7e6d65f0afa09773e061d5c723
moniquebtw <moniquesilva14321@gmail.com>
2025-04-02 18:14:12 -0300

    ajuste na tela blog

--- js/blog.js
+++ js/blog.js
@@ -3,7 +3,7 @@ const imagemBlog = ['about_blog-1.jpg', 'about_blog_2.jpg', 'about_blog-3.jpg', 
 const tituloBlog = ['Porttitor pharetra, consectetur viverra est nisl a, vulputate id.', 'Diam amet non placerat risus magna non volutpat.', 'Volutpat volutpat turpis risus nunc in nisl habitasse leo. Duis.', 'Porttitor pharetra, consectetur viverra est nisl a, vulputate id.'];
 const dataBlog = ['13 Sept, 2021', '13 Sept, 2021', '13 Sept, 2021', '13 Sept, 2021'];
 const tipoBlog = ['Technology', 'Technology', 'Technology', 'Technology'];
-const criandoUmBlog = document.querySelector('.card_blog')
+const criandoUmBlog = document.querySelector('.card_blog_teste')
 
 const shortcutImagens = ['shortcut_blog-1.jpg', 'shortcut_blog-2.jpg', 'shortcut_blog-3.jpg', 'shortcut_blog-4.jpg'];
 const shortcutTitulos = ['Faucibus interdum cras imperdiet fames. Ullamcorper.', 'Faucibus interdum cras imperdiet fames. Ullamcorper.', 'Faucibus interdum cras imperdiet fames. Ullamcorper.', 'Faucibus interdum cras imperdiet fames. Ullamcorper.'];
@@ -11,11 +11,11 @@ const criandoUmShortcut = document.querySelector('.card_blog_dois')
 
 for (let i = 0; i < 4; i++) {
     const novoBlog = document.createElement('div');
-    novoBlog.className = ('novoBlog');
+    novoBlog.className = ('novoBlog_teste');
     novoBlog.innerHTML = `
         <img class="novoBlog" src="/assets/${imagemBlog[i]}" alt="${imagemBlog[i]}">
         <h4>${tituloBlog[i]}</h4>
-        <div class="d-flex align-items-center justify-content-between">
+        <div class=texto_do_blog>
         <span>${dataBlog[i]}</span> <span>${tipoBlog[i]}</span>
         </div>
 
@@ -31,10 +31,12 @@ for (let i = 0; i < 4; i++) {
         <div class="imagem-e-titulo">
             <img class="shortcut-image" src="/assets/${shortcutImagens[i]}" alt="${shortcutImagens[i]}">
             <h4>${shortcutTitulos[i]}</h4>
-        </div>
-        <div class="d-flex align-items-center justify-content-end">
+            <div class="d-flex align-items-center justify-content-end">
             <span>${dataBlog[i]}</span> <span>${tipoBlog[i]}</span>
         </div>
+        </div>
+        
+        
     `;
 
     criandoUmShortcut.appendChild(novoShortcut);
